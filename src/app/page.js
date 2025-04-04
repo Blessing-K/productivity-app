52acf98e80dbe2371d8df088e63e51bc65e3b520
Blessing-K <boluwatitobee@gmail.com>
2025-04-04 17:56:28 -0400

    Removed unnecessary comments and file to be ready to submit project phase1

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,5 @@
 "use client"
 
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-// import jwt from "jsonwebtoken";
-// import QuoteBanner from "@/src/components/QuoteBanner";
-
-// export default function Home() {
-//   const router = useRouter();
-
-//   return (
-//     <div style={{ textAlign: "center", padding: "2rem" }}>
-//       <h1>Welcome to Your Productivity App</h1>
-//       <p>You are logged in!</p>
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import jwt from "jsonwebtoken";
